5d58708aff47c3958f43fcb599d5aff5aeea7c39
jinyonghui <real_jyh@163.com>
2019-01-02 16:47:18 +0800

    ideaConfig文件

---
+++ config/fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author : jinyonghui 
+ * Date : ${DATE} ${TIME}
+ * Desc : ${DESCRIPTION}
+ */
+public class ${NAME} {
+}
